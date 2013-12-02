813ae9e05c4f5fbfee281588477e52a8baf1ac3a
AlecBayliff <ab5tb@mst.edu>
2013-12-01 21:47:48 -0600

    Added constants to main

--- main.cpp
+++ main.cpp
@@ -13,23 +13,32 @@ using namespace std;
 
 int main()
 {
-  Customer customers[20];
-  Customer tempc[20];
-  int num = 0;
+  const int MAX_HAP = 90;
+  const int MIN_HAP = 10;
+  Customer customers[NUM_CUSTOMERS];
+  Customer tempc[NUM_CUSTOMERS];
+  int num = NUM_CUSTOMERS;
 
-  Customer::readCustomers(customers, 20);
+  Customer::readCustomers(customers, NUM_CUSTOMERS);
 
-  for(int i = 0 ; i < 20 ; i ++)
+  for(int i = 0 ; i < NUM_CUSTOMERS ; i ++)
     cout << customers[i];
 
-  return 0;
-  Business moes("Moe's Bar", 0, 20);
-  Business comics("Comic Book Store, 0, 20);
+  Business moes("Moe's Bar", 0, NUM_CUSTOMERS);
+  Business comics("Comic Book Store, 0, NUM_CUSTOMERS);
   Customer street[20];
   
   do
   {
-    for(int i = 0; i < num_customers; i++)
+    for(int i = 0; i < num; i++)
+    {
+      if(street[i].happiness < MIN_HAP || street[i].happiness > MAX_HAP)
+      {
+        street[num] = tempc[i];
+        tempc[i] = street[i];
+        num--;
+      }
+    for(int i = 0; i < num; i++)
     {
       if(inclination = false)
       {
@@ -44,7 +53,9 @@ int main()
 
     moes.sellStuff();
     comics.sellStuff();
-  
+    
+    num = 0;
+    
     moes.customersLeave(street, num);
     comics.customersLeave(street, num);
   
