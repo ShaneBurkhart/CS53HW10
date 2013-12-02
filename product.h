49b6cc9b75ec5cbed26e198e5674503fc4dc7ded
Shane Burkhart <shaneburkhart@gmail.com>
2013-12-01 18:47:34 -0600

    Product struct added

---
+++ product.h
@@ -0,0 +1,20 @@
+//////////////////////////////////////////////////////////////////////////////
+// Programer: Shane Burkhart         Student ID: 99999
+// Assignment: 9 "business"          Filename: main.cpp
+// Due Date: 11/12/13                Class: CS53, Section E
+// Desc: Program that tests the business and customer classes
+//
+
+#ifndef PRODUCT_H
+#define PRODUCT_H
+
+#include<string>
+using namespace std;
+
+struct Product
+{
+  string name;
+  float price;
+};
+
+#endif
