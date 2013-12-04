d02b25d324f45cc42052c6b07a8e1fd27b5cede3
AlecBayliff <ab5tb@mst.edu>
2013-12-03 23:07:51 -0600

    Update business.h

--- business.h
+++ business.h
@@ -1,7 +1,8 @@
 //////////////////////////////////////////////////////////////////////////////
 // Programer: Shane Burkhart         Student ID: 99999
 //            Brendan Nulsen
-// Assignment: 9 "business"          Filename: main.cpp
+//            Alec Bayliff
+// Assignment: 10 "Street Brawl"     Filename: main.cpp
 // Due Date: 11/12/13                Class: CS53, Section E
 // Desc: Header file for business class
 //
