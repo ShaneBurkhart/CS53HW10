f3a6d28d467c81069dc9f76d6bfb329bd7385df4
AlecBayliff <ab5tb@mst.edu>
2013-12-03 23:13:13 -0600

    Updated the comment header

--- product.cpp
+++ product.cpp
@@ -1,3 +1,11 @@
+//////////////////////////////////////////////////////////////////////////////
+// Programer: Shane Burkhart         Student ID: 99999
+//            Brendan Nulsen
+//            Alec Bayliff
+// Assignment: 10 "Street Brawl"     Filename: product.cpp
+// Due Date: 11/12/13                Class: CS53, Section E
+// Desc: Implementation file for the product struct
+
 #include<iostream>
 #include "product.h"
 using namespace std;
